d8f8c994df14c2e05ef43e417ae4779757adfe61
Denis Badurina <badurinadenis@gmail.com>
2020-04-27 11:15:23 +0200

    refactor: use ssh-identity over deploy-key & drop unnecessary size log

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,25 +2,23 @@
 set -eu
 
 # set up the deploy key
-DEPLOY_KEY=$1
-if [ -z "$DEPLOY_KEY" ]; then
-  echo "Deploy key is required."
+SSH_IDENTITY=$1
+if [ -z "$SSH_IDENTITY" ]; then
+  echo "SSH identity is required"
   exit 1
 fi
 SSH_PATH="$HOME/.ssh"
 mkdir -p "$SSH_PATH"
-echo "$DEPLOY_KEY" > "$SSH_PATH/deploy_key"
-chmod 600 "$SSH_PATH/deploy_key"
+echo "$SSH_IDENTITY" > "$SSH_PATH/SSH_IDENTITY"
+chmod 600 "$SSH_PATH/SSH_IDENTITY"
 
 # paths
 FROM=$2
 TO=$3
 if [ -z "$FROM" ] || [ -z "$TO" ]; then
-  echo "From and to paths are required."
+  echo "From and to paths are required"
   exit 1
 fi
 
 # do rsync
-FROM_SIZE=$(du -sh .git | awk '{print $1}')
-echo "Copying $FROM_SIZE of files from $FROM to $TO"
-sh -c "rsync -avhO --no-perms -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$FROM $TO"
+sh -c "rsync -avhO --no-perms -e 'ssh -i $SSH_PATH/SSH_IDENTITY -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$FROM $TO"
